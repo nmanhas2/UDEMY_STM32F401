45270875ed10ed108e125e3e06b57b6911ef047b
nmanhas2 <nubal.manhas1@gmail.com>
2023-12-18 22:57:53 -0700

    initial

---
+++ 9_adc_single_conversion/Inc/adc.h
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////////////////////////
+
+/// Using: STM32F401 NUCLEO Board
+
+///	Following Section 5: ADC (https://www.udemy.com/course/embedded-systems-bare-metal-programming/)
+
+///	Purpose: Creating a Library for ADC
+
+/// Author: Nubal Manhas
+
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef INC_ADC_H_
+#define INC_ADC_H_
+
+#include <stdint.h>
+void pa1_adc_init(void);
+uint32_t adc_read(void);
+void start_conversion(void);
+
+#endif /* INC_ADC_H_ */
